c1d07f80d11ad9c585fb6ab2b73fbcde4e10914c
Benjamin Darcet <b.darcet@gmail.com>
2018-11-23 11:08:20 +0100

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,8 +7,17 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save
-    redirect_to cocktail_path(@dose.cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'doses/new'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
+  delete 'doses/:id', to: 'doses#destroy', as: "dose_delete"
 end
